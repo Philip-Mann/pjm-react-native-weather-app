8537902206ac270d0f0fa1b5c33d6ac01f30faea
Philip Mann <mannphilip7@gmail.com>
2022-03-07 17:39:22 -0700

    commented out Input.js and am now working in App.js

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, TextInput, View } from 'react-native';
 import Input from './components/Input';
 
 export default function App() {
-
   
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const api = {
+    url: "https://api.openweathermap.org/data/2.5/",
+    key: "2e7d702ab251babde4b714ed88c48e60"
+  }
+
+  const handleFetch = (query) => {
+    // setSearchQuery(query);
+    fetch(`${api.url}weather?q=${query}&units=imperial&appid=${api.key}`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
+  // TODO: Capture text input and use it to fetch
+  // READ THIS: https://stackoverflow.com/questions/60091873/how-to-get-values-from-textinput
+
   return (
     <View >
-      <Input/>
+      <TextInput
+                style={styles.input}
+                onEndEditing={(query) => handleFetch(query)}
+                placeholder="Search by City"
+                defaultValue={searchQuery}
+                keyboardType='default'
+      />
     </View>
   );
 }
@@ -18,5 +41,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
